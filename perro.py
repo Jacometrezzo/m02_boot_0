a64d2803adcc476783dd9d3cdea6dfce4837e6ec
Victor Fernandez Marcilla <VictorFernandezMarcilla@Mac-mini.home>
2019-01-20 18:57:41 +0100

    Herencia y sobreescritura

--- perro.py
+++ perro.py
@@ -8,12 +8,35 @@ class Perro():
         if self.peso >= 8:
             print('GUAU GUAU')
         else:
-            print('Guau, Guau')
+            print('ay, ay')
             
     def __str__(self):
         return 'Perro {}, edad: {}, peso: {}'.format(self.nombre, self.edad, self.peso)
     
+class PerroAsistencia(Perro):
+    def __init__(self, nombre, edad, peso, amo):
+        Perro.__init__(self, nombre, edad, peso)
+        self.amo = amo
+        self.__trabajando = False
+        
+    def __str__(self):
+        return 'Perro de asistencia de {}'.format(self.amo)
     
+    def pasear(self):
+        print('{} ayudo a mi dueño, {} a pasear'.format(self.nombre, self.amo))
+        
+    def ladrar(self):
+        if self.__trabajando:
+            print('Soy {} y ahora no puedo ladrar porque estoy trabajando para mi amo {}'.format(self.nombre, self.amo))
+        else:
+            Perro.ladrar(self)
+
+    def trabajando(self, valor=None):
+        if valor == None:
+            return self.__trabajando
+        else:
+            self.__trabajando = valor
+        
         
     
         
